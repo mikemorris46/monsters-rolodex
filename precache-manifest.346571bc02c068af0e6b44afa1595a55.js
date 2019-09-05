9df12271d0caf97fe33f3ed9f5b2838c5ffb217b
mikemorris46 <mike.morris.wa@gmail.com>
2019-09-05 12:21:26 +0800

    Updates

---
+++ precache-manifest.346571bc02c068af0e6b44afa1595a55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aed8138915b9e433080e3e01e7c73ce6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b265ab2fa0695ee8d6ca",
+    "url": "/monsters-rolodex/static/css/main.dab886df.chunk.css"
+  },
+  {
+    "revision": "ed0b678595ac837088bc",
+    "url": "/monsters-rolodex/static/js/2.67bd6e0c.chunk.js"
+  },
+  {
+    "revision": "b265ab2fa0695ee8d6ca",
+    "url": "/monsters-rolodex/static/js/main.efdc057b.chunk.js"
+  },
+  {
+    "revision": "9ea9c29140c1f80c8044",
+    "url": "/monsters-rolodex/static/js/runtime~main.3810dc4c.js"
+  }
+]);
