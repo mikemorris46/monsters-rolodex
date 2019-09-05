ba21da6ebec4d9e0352e1090b3509d305c8145df
mikemorris46 <mike.morris.wa@gmail.com>
2019-09-05 12:34:54 +0800

    initial commit of project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import { CardList } from './components/card-list/card-list.component'
+import { SearchBox } from './components/search-box/search-box.component'
 
-function App() {
+import './App.css'
+
+let filteredMonsters = []
+
+const App = () => {
+  const [monsters, setMonsters] = useState([])
+  const [searchField, setSearchField] = useState('')
+
+  const handleSearchChange = e => {
+    setSearchField(e.target.value)
+    const mySearch = e.target.value
+
+    filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(mySearch.toLowerCase())
+    )
+  }
+
+  const fetchData = async () => {
+    try {
+      let response = await fetch(`https://jsonplaceholder.typicode.com/users`)
+      let data = await response.json()
+      setMonsters(data)
+    } catch (error) {
+      console.log('Error: ', error)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Monsters Rolodex</h1>
+      <SearchBox
+        placeholder='search monsters'
+        handleChange={e => handleSearchChange(e)}
+      />
+      {filteredMonsters.length > 0 ? (
+        <CardList monsters={filteredMonsters} />
+      ) : (
+        <CardList monsters={monsters} />
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/search-box/search-box.component.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './search-box.styles.css'
+
+export const SearchBox = ({ placeholder, handleChange }) => {
+  return (
+    <div>
+      <input
+        type='search'
+        className='search'
+        placeholder={placeholder}
+        onChange={handleChange}
+      />
+    </div>
+  )
+}
